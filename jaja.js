72d33bdbcde12c63743e4fbfc7ea01aa89b4991b
Yremancer <chilumy@mail.ru>
2024-04-24 18:34:10 +1000

    test
    test request

---
+++ jaja.js
@@ -0,0 +1,14 @@
+// const calculateSum = (args) => {
+//     let sum = 0;
+//     for (item of args) {
+//         if (typeof(item.price) === 'number'){
+//             if (item.price > 0){
+//                 sum += item.price;
+//             }
+//         }
+//         else{return 'Error';}
+//     }
+//     return sum;
+// }
+
+
